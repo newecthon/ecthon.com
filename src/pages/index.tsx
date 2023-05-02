54365d72edcd77b20b2010c3e330bd3a61806e3b
newecthon <ecthon@gmail.com>
2023-05-02 20:06:06 -0300

    reset config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Navbar } from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      <h1>Hello</h1>
     </>
   )
 }
