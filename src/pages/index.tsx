e99bd6fc21acb92e4540263ea940d461befdbac1
newecthon <ecthon@gmail.com>
2023-05-03 16:29:40 -0300

    feat/components: Heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
 'use client'
 
 import { Header } from '@/components/Header'
-import { Text } from '@chakra-ui/react'
+import { Heading } from '@/components/Heading'
+import { Flex, Text } from '@chakra-ui/react'
 
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <Flex
+      w='100%'
+      h='100vh'
+      flexDir='column'
+      bg='#1C1C1C'
+    >
       <Header />
-    </>
+      <Heading />
+    </Flex>
   )
 }
