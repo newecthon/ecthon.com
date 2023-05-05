f271045c14f570cd57a72f4983183890ab5526aa
newecthon <ecthon@gmail.com>
2023-05-04 21:21:17 -0300

    style/Header: created hover effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
 import { Link } from "@chakra-ui/next-js";
-import { Flex } from "@chakra-ui/react";
+import { Flex, LinkBox } from "@chakra-ui/react";
 import { NavItem } from "./NavItem";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 export function Header() {
+    const pages = [
+        "Home",
+        "About",
+        "Contato"
+    ]
+    const [selectedTab, setSelectedTab] = useState(pages[0])
+
     return (
         <Flex
             w='100%'
@@ -10,28 +19,52 @@ export function Header() {
             align='center'
             justify='center'
             background='#1C1C1C'
-            borderBottom='1px solid #282828'
         >
             <Flex as='header'
                 minW='1120px'
-                h='100%'
                 justifyContent='space-between'
-                align='center'
-                // borderBottom='1px solid #282828'
             >
                 <Link href='/'>E</Link>
 
                 <Flex as='nav'
                     gap='16px'
                 >
-                    <NavItem path="/" label="Home"/>
-                    <NavItem path="/" label="Stack"/>
-                    <NavItem path="/" label="Portfolio"/>
-                    <NavItem path="/" label="Contato"/>
+                    {pages.map(item => (
+                        <Link href={item} color='#fff'
+                            background={item === selectedTab ? "red" : ""}
+                            padding='20px'
+                            textDecor='none'
+                            transition='color 0.3s ease-in-out'
+                            _hover={{
+                                color: 'red'
+                            }}
+                            _after={{
+                                content: '""',
+                                background: '#00f3f4',
+                                opacity: 0,
+                                transition: 'opacity 0.3s ease-in-out'
+                            }}
+                            onMouseMove={() => setSelectedTab(item)}
+                        >
+                            {item}
+                            {item === selectedTab ? (
+                                <motion.div style={{ background: 'blue' }} layoutId="underline"/>
+                            ) : null}
+                        </Link>
+                    ))}
+                    
                 </Flex>
-
             </Flex>
-            
+            <AnimatePresence>
+                <motion.div
+                    layoutId="nav"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    // transition={{ duration: 0.3 }}
+                >
+                </motion.div>
+            </AnimatePresence>
         </Flex>
     )
 }
