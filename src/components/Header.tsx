3ce7821ebc5c464747e9afc1664351c477f9a16d
newecthon <ecthon@gmail.com>
2023-05-04 22:05:06 -0300

    remove component NavItem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { Link } from "@chakra-ui/next-js";
 import { Flex, LinkBox } from "@chakra-ui/react";
-import { NavItem } from "./NavItem";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
@@ -24,47 +23,25 @@ export function Header() {
                 minW='1120px'
                 justifyContent='space-between'
             >
-                <Link href='/'>E</Link>
+                <Link href='/' color='#fff' fontSize='40px'>ecthon</Link>
 
                 <Flex as='nav'
                     gap='16px'
                 >
                     {pages.map(item => (
-                        <Link href={item} color='#fff'
+                        <Link key={item} href={item} color='#fff'
                             background={item === selectedTab ? "red" : ""}
                             padding='20px'
                             textDecor='none'
                             transition='color 0.3s ease-in-out'
-                            _hover={{
-                                color: 'red'
-                            }}
-                            _after={{
-                                content: '""',
-                                background: '#00f3f4',
-                                opacity: 0,
-                                transition: 'opacity 0.3s ease-in-out'
-                            }}
                             onMouseMove={() => setSelectedTab(item)}
                         >
                             {item}
-                            {item === selectedTab ? (
-                                <motion.div style={{ background: 'blue' }} layoutId="underline"/>
-                            ) : null}
                         </Link>
                     ))}
                     
                 </Flex>
             </Flex>
-            <AnimatePresence>
-                <motion.div
-                    layoutId="nav"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    // transition={{ duration: 0.3 }}
-                >
-                </motion.div>
-            </AnimatePresence>
         </Flex>
     )
 }
