baf96b809cfcea0e979c58fce74a7363ff90aa26
newecthon <ecthon@gmail.com>
2023-05-08 20:19:53 -0300

    feat - cards projects

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,24 +1,35 @@
 import { Divider, Flex, Image, Link, Text } from "@chakra-ui/react";
 import { ArrowSquareOut } from "@phosphor-icons/react";
+import { color } from "framer-motion";
 import { useState } from "react";
 
 const projetos = [
     {
-        name: 'Name Project',
-        type: 'personal',
-        stack: 'Front-End',
+        name: 'Name One',
+        type: 'Rocket',
+        stack: 'UI Design',
         url: 'https://framerusercontent.com/images/mS2ifdJRDbVaHbj8DUqxLF76kBo.webp'
     },
     {
-        name: 'Name Project',
-        type: 'personal',
+        name: 'Name Tow',
+        type: 'Personal',
         stack: 'Front-End',
-        url: 'https://framerusercontent.com/images/mS2ifdJRDbVaHbj8DUqxLF76kBo.webp'
+        url: 'https://framerusercontent.com/images/eHiRi6YTJ3lMGpQgXUYbrCPuFJM.webp'
+    },
+    {
+        name: 'Name Tow',
+        type: 'Personal',
+        stack: 'Front-End',
+        url: 'https://framerusercontent.com/images/eHiRi6YTJ3lMGpQgXUYbrCPuFJM.webp'
     }
 ]
 
 export function Heading() {
-    const [ isSelected, setIsSelected] = useState('');
+    const [ isSelected, setIsSelected] = useState(false)
+
+    function handleEffect() {
+        setIsSelected(!isSelected)
+    }
 
     return (
         <Flex
@@ -41,110 +52,112 @@ export function Heading() {
                 gap='32px'
             >
                 {/* CARD */}
-                <Flex
-                    w='352px'
-                    h='auto'
-                    bg='cardbg'
-                    borderRadius='10px'
-                    direction='column'
-                    overflow='hidden'
-                    border='1px solid #282828'
-                    cursor='pointer'
-                >
-                    <Flex>
-                        <Image
-                            h='100%' 
-                            src='https://framerusercontent.com/images/mS2ifdJRDbVaHbj8DUqxLF76kBo.webp'
-                            _hover={{
-                                filter: 'blur(2px)'
-                            }}
-                            />
-                    </Flex>
+                {projetos.map((projet, index) => (
                     <Flex
-                        h='auto'
-                        p='20px'
+                        key={index}
+                        w='352px'
+                        h='382px'
+                        bg='cardbg'
+                        justifyContent='space-between'
+                        borderRadius='10px'
                         direction='column'
-                        bg='#161616'
+                        overflow='hidden'
+                        border='1px solid #282828'
+                        cursor='pointer'
+                        onMouseMove={handleEffect}
                     >
-                        <Flex
-                            justify='space-between'
-                        >
-                            <Text
-                                color='#fff'
-                                fontWeight='600'
-                                mb='4px'
-                            >
-                                Name Project
-                            </Text>
-                            <Link
-                                display='flex'
-                                color='#83868A'
-                                gap='4px'
-                                transition='0.2s'
-                                _hover={{
-                                    color: '#FFF'
-                                }}
-                            >
-                                <ArrowSquareOut size={24} />
-                            </Link>
+                        <Flex>
+                            <Image
+                                h='100%' 
+                                src={projet.url}
+                                />
                         </Flex>
-
                         <Flex
-                            align='center'
-                            gap='8px'
-                            color="#83868A"
+                            h='auto'
+                            p='20px'
+                            direction='column'
+                            bg='#161616'
                         >
-                            <Text fontSize='14px'>Personal</Text>
-                            <Flex w='4px' h='1' borderRadius='50px' bg='#83868A'/>
-                            <Text fontSize='14px'>Front-End</Text>
-                        </Flex>
-
-                        <Flex
-                            justify='space-between'
-                            gap='16px'
-                            mt='16px'
-                        >
+                            <Flex
+                                justify='space-between'
+                            >
+                                <Text
+                                    color='#fff'
+                                    fontWeight='600'
+                                    mb='4px'
+                                >
+                                    {projet.name}
+                                </Text>
                                 <Link
-                                    textAlign='center'
-                                    textDecor='none'
-                                    width='100%'
-                                    padding='4px 16px'
-                                    border='1px solid transparent'
-                                    borderRadius='8px'
-                                    mt='8px'
+                                    display='flex'
                                     color='#83868A'
+                                    gap='4px'
                                     transition='0.2s'
                                     _hover={{
-                                        textDecor: 'none',
-                                        color: '#FFF',
-                                        border: '1px solid #282828',
-                                        background: '#1C1C1C'
+                                        color: '#FFF'
                                     }}
                                 >
-                                    Detalhes
+                                    <ArrowSquareOut size={24} />
                                 </Link>
-                                <Link
-                                    textAlign='center'
-                                    textDecor='none'
-                                    width='100%'
-                                    padding='4px 16px'
-                                    border='1px solid transparent'
-                                    borderRadius='8px'
-                                    mt='8px'
-                                    color='#83868A'
-                                    transition='0.2s'
-                                    _hover={{
-                                        textDecor: 'none',
-                                        color: '#FFF',
-                                        border: '1px solid #282828',
-                                        background: '#1C1C1C'
-                                    }}
-                                >
-                                    Online ⚡
-                                </Link>
+                            </Flex>
+
+                            <Flex
+                                align='center'
+                                gap='8px'
+                                color="#83868A"
+                            >
+                                <Text fontSize='14px'>{projet.type}</Text>
+                                <Flex w='4px' h='1' borderRadius='50px' bg='#83868A'/>
+                                <Text fontSize='14px'>{projet.stack}</Text>
+                            </Flex>
+
+                            <Flex
+                                justify='space-between'
+                                gap='16px'
+                                mt='16px'
+                            >
+                                    <Link
+                                        textAlign='center'
+                                        textDecor='none'
+                                        width='100%'
+                                        padding='4px 16px'
+                                        border='1px solid transparent'
+                                        borderRadius='8px'
+                                        mt='8px'
+                                        color='#83868A'
+                                        transition='0.2s'
+                                        _hover={{
+                                            textDecor: 'none',
+                                            color: '#FFF',
+                                            border: '1px solid #282828',
+                                            background: '#1C1C1C'
+                                        }}
+                                    >
+                                        Detalhes
+                                    </Link>
+                                    <Link
+                                        textAlign='center'
+                                        textDecor='none'
+                                        width='100%'
+                                        padding='4px 16px'
+                                        border='1px solid transparent'
+                                        borderRadius='8px'
+                                        mt='8px'
+                                        color='#83868A'
+                                        transition='0.2s'
+                                        _hover={{
+                                            textDecor: 'none',
+                                            color: '#FFF',
+                                            border: '1px solid #282828',
+                                            background: '#1C1C1C'
+                                        }}
+                                    >
+                                        Online ⚡
+                                    </Link>
+                            </Flex>
                         </Flex>
                     </Flex>
-                </Flex>
+                ))}
             </Flex>
 
         </Flex>
