06edc65d32a76e33c88b6b2cfbc8b379e4035f61
newecthon <ecthon@gmail.com>
2023-05-08 11:27:03 -0300

    Edit global styles

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,12 +1,23 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
-    colors: {},
+    // global styles
+    styles: {
+        global: {
+            body: {
+                bg: 'bggray',
+            }
+        }
+    },
+    // colors
+    colors: {
+        bggray: '#161616'
+    },
+    // external fonts
     fonts: {
         heading: ``,
         body: `'Bai Jamjuree', sans-serif`,
     },
-    global: {}
 })
 
 export default theme
