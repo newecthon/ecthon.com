ef1c12c69df7ee7be62bb6a37a1e5f8a70400e5a
newecthon <ecthon@gmail.com>
2023-05-12 00:32:08 -0300

    feat: created component Technologies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Heading } from '@/components/Heading'
 import { Projects } from '@/components/Projects'
+import { Technologies } from '@/components/Technologies'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Heading />
+      <Technologies />
       <Projects />
       <Footer />
     </>
